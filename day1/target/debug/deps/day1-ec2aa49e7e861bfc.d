41cff3a5b7d34c226870dfd4ec0e74464ddf5bdc
Dennis Wambua <dennis.wambua@gmail.com>
2022-07-19 09:58:29 +0300

    Repo cleanup

--- day1/target/debug/deps/day1-ec2aa49e7e861bfc.d
+++
@@ -1,5 +0,0 @@
-/Users/denniswambua/coding/AdventOfCodeChallenge/day1/target/debug/deps/day1-ec2aa49e7e861bfc: src/main.rs
-
-/Users/denniswambua/coding/AdventOfCodeChallenge/day1/target/debug/deps/day1-ec2aa49e7e861bfc.d: src/main.rs
-
-src/main.rs:
